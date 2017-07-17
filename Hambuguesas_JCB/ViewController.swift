d8d4654f776a00ae8c287ba98a352a8c15f7da91
JorgeCB78 <jcarrillob78@hotmail.com>
2017-07-17 00:07:47 -0500

    Initial Commit

---
+++ Hambuguesas_JCB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hambuguesas_JCB
+//
+//  Created by Jorge Carrillo Balcazar on 17/07/17.
+//  Copyright © 2017 JCarrilloB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
